bf107a66263d44fd67fec30846400099d3ff5b35
Titus Ouko <titusouko@gmail.com>
2019-12-05 11:03:00 +0300

    Add new django project configurations

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/moringa/Documents/moringa-school-projects/allProjects/housing/virtual/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

6a0dab367d671b2ed4fd8b233246af51a31a3c35
Titus Ouko <titusouko@gmail.com>
2019-12-09 13:38:00 +0300

    Add model methods

--- houses/models.py
+++ houses/models.py
@@ -3,13 +3,20 @@ from django.contrib.auth.models import User
 from pyuploadcare.dj.models import ImageField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db.models import Q
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     bio = models.TextField()
     email =models.EmailField(blank=True)
-    profile_pic= models.ImageField(upload_to='images/',default='media/avatar.png', blank=True)
+    profile_pic= models.ImageField(upload_to='images/',default='images/avatar.png', blank=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_save(self):
+        self.delete()
 
     def __str__(self):
         return f'{self.name}'
@@ -38,16 +45,22 @@ class House(models.Model):
     house_pic = models.ImageField(upload_to='media/')
     house_type = models.CharField(max_length=100)
     no_of_rooms = models.CharField(max_length=5)
-    year_built = models.CharField(max_length=5)
+    
     price = models.CharField(max_length=100, default=0)
     owner = models.ForeignKey('Owner', on_delete=models.CASCADE)
     date_posted = models.DateTimeField(auto_now_add=True)
+    
     
     def save_house(self):
         self.save()
     
     def delete_house(self):
         self.delete()
+
+    @classmethod
+    def search(cls,searchterm):
+        search = cls.objects.filter(house_location__icontains=searchterm)
+        return search
 
     def __str__(self):
         return f'{self.house_no}'
@@ -61,7 +74,12 @@ class Owner(models.Model):
     address = models.CharField(max_length=100)
     email = models.EmailField(blank=True)
     phone = models.PositiveIntegerField()
-    owner_pic = models.ImageField(upload_to='images/', default='images/avatar')
+    owner_pic = models.ImageField(upload_to='images/', default='images/avatar.png')
 
+    def save_owner(self):
+        self.save()
+
+    def delete_owner(self):
+        self.delete()
     def __str__(self):
         return f'{self.first_name}'
